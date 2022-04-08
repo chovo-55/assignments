3546126aed665bf89478329d9fee93b4dde1902e
chovo <chovo195@gmail.com>
2022-04-08 12:30:07 +0200

    maeven

--- Maeven/src/main/java/Generics.java
+++ Maeven/src/main/java/Generics.java
@@ -1,2 +1,15 @@
-public class Generics {
+import java.util.HashMap;
+import java.util.Map;
+
+public class Generics <K,V>{
+    K key;
+    V value;
+
+    Map<K, V> map = new HashMap<>();
+
+    void printType(K key, V value){
+        map.put(key, value);
+        System.out.println(map.get(key));
+    }
+
 }
