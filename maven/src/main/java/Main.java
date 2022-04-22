f7c6a2a7ffb670890032fc6a9e572572fc323777
chovo <chovo195@gmail.com>
2022-04-22 12:05:23 +0200

    spring

--- maven/src/main/java/Main.java
+++ maven/src/main/java/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
         Database service = new Database();
         //service.updateCommon("COMMON",1,"sad",123.3,543.1,null,null,null);
+
         service.insertIntoCommon("COMMON",6445,"abda",213.231,5634.231,null,null,null);
     }
 }
